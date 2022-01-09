b85ac776a5ac56ffff51d4e166c8b20f35839acd
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2022-01-09 10:47:25 -0300

    Primeiro Commit como Master

---
+++ src/main/kotlin/com/spedison/planetasstarwars/PlanetasStarWarsApplication.kt
@@ -0,0 +1,11 @@
+package com.spedison.planetasstarwars
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanetasStarWarsApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanetasStarWarsApplication>(*args)
+}
